7bcdcaff43e409bf693f12696b8f995c78dced60
unknown <nageswar.bodduri@SG-LT-023.senecaglobal.net>
2016-10-11 19:13:30 +0530

    Added new code to validate non-english language in preferences tab

---
+++ src/main/java/com/webmailhostopia/common/utils/Verfiy.java
@@ -0,0 +1,67 @@
+package com.webmailhostopia.common.utils;
+
+import java.text.Collator;
+import java.util.Locale;
+
+public class Verify {
+
+	public static boolean verifyArrayofStrings(String[] actual, String[] expected, boolean ignorecase){
+		boolean isTrue=true;
+		if(actual.length!=expected.length)
+		{
+			Log.info("Actual and Expected arrays Lengths are not equal: Actual: "+ actual.length+ "  Expected: "+expected.length);
+			return false;
+		}
+		else
+		{
+			for(int i=0;i<actual.length;i++)
+			{
+				if(ignorecase){
+					if(actual[i].equalsIgnoreCase(expected[i])){
+						Log.info("Actual and Expected string are equal. Actual: "+ actual[i]+ "  Expected: "+expected[i]);
+						continue;
+					}
+					else{
+						isTrue=false;
+						Log.info("Actual and Expected string are not equal. Actual: "+ actual[i]+ "  Expected: "+expected[i]);
+					}
+				}
+				else
+				{
+					if(actual[i].equals(expected[i])){
+						Log.info("Actual and Expected string are equal. Actual: "+ actual[i]+ "  Expected: "+expected[i]);
+						continue;
+					}
+					else{
+						isTrue=false;
+						Log.info("Actual and Expected string are not equal. Actual: "+ actual[i]+ "  Expected: "+expected[i]);
+					}
+				}
+			}
+		}
+		return isTrue;
+	}
+
+	public static boolean compareStringInNonEnglish(String[] actual, String[] expected)throws Exception{
+
+		boolean isTrue=true;
+		Collator collator = Collator.getInstance(Locale.FRENCH);
+
+		if(actual.length!=expected.length){
+			Log.info("Actual and Expected arrays Lengths are not equal: Actual: "+ actual.length+ "  Expected: "+expected.length);
+			return false;
+		}else{
+			for(int i=0;i<actual.length;i++)
+			{
+				if(collator.compare(actual[i].trim(), expected[i].trim()) == 0){
+					Log.info("Actual and Expected string are equal. Actual: "+ actual[i]+ "  Expected: "+expected[i]);
+					continue;
+				}else{
+					isTrue=false;
+					Log.error("Actual and Expected string are not equal. Actual: "+ actual[i]+ "  Expected: "+expected[i]);
+				}
+			}
+			return isTrue;
+		}
+	}
+}
--- src/main/java/com/webmailhostopia/testimpl/LanguageTest.java
+++ src/main/java/com/webmailhostopia/testimpl/LanguageTest.java
@@ -1,52 +1,90 @@
 package com.webmailhostopia.testimpl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
-
 import com.webmailhostopia.common.utils.ByLocator;
-import com.webmailhostopia.common.utils.LanguageType;
+import com.webmailhostopia.common.utils.Log;
+import com.webmailhostopia.common.utils.Verify;
 import com.webmailhostopia.selenium.webdriver.AbstractPageObject;
-import com.webmailhostopia.selenium.webdriver.WebDriverType;
 
 public class LanguageTest extends AbstractPageObject{
-	
-	
-	public static boolean languageTest() throws Exception{
-		int failCounter = 0;
-		int i=0;
-		
-		String[] sideNavBar = otherLangTestData.getProperty("").split(",");
-		
+
+
+	public static boolean labelsLanguageTestInPreferenceTab() throws Exception{
+
+		List<String> lsStr = new ArrayList<String>();
+
+		String[] sideNavBar = otherLangTestData.getProperty("SIDE_NAV_BAR_MENU_ITEMS_FRENCH").split(",");
+
 		openPreferencesTab();
 		changeTheLanguage();
 		//side navigation bar text validation
-		List<WebElement> ls = driver.findElements(By.xpath("//*[contains(@class,'nav navbar-nav side-nav1')]/li"));
-		
-		
+		List<WebElement> ls = driver.findElements(By.xpath(elementLocatorProp.getProperty("SIDE_NAV_BAR_ELE_XPATH")));
+
+		Log.info("Side navigation bar labels:");
 		for(WebElement we:ls){
-			if(!we.getText().equalsIgnoreCase(sideNavBar[i])){
-				failCounter++;
-			}
+			String tmp = we.getText().trim();
+			lsStr.add(tmp);
 		}
-		return (failCounter==0)?true:false;
+		String[] actuals = new String[lsStr.size()];
+		actuals = lsStr.toArray(actuals);
+
+		boolean testResult1 = Verify.compareStringInNonEnglish(actuals, sideNavBar);
+		boolean testResult2 = labelsTestInPreferenceMainActivityPage();
+		return testResult1 && testResult2;
 	}
-	
+
 	public static void openPreferencesTab()throws Exception{
-		WebElement preferences = waitForElementToBeClickable(ByLocator.xpath, "//a[contains(@href,'preferences')]", 60);
+		Log.info("Opening preferences tab...");
+		WebElement preferences = waitForElementToBeClickable(ByLocator.xpath, elementLocatorProp.getProperty("PREFERENCE_BUTTON_XPATH"), 60);
 		if( preferences != null){
 			preferences.click();
+			waitForElementToBeClickable(ByLocator.css, elementLocatorProp.getProperty("PREF_HEADER_BAR"), 30);
+			Log.info("Successfully opened preferences tab!!");
 		}
-		
+
 	}
-	
+
 	public static void changeTheLanguage()throws Exception{
-		Select langDropDown = new Select(driver.findElement(By.id("language")));
-		langDropDown.selectByValue(language);
+		Log.info("Selecting language to test");
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//label[@translate='LANG_PREFERENCES_GENERAL_FORM_LANGUAGE']/../div")).click();
+
+		WebElement lan = waitForElementToBeClickable(ByLocator.xpath, "//*[@translate='LANG_PREFERENCES_GENERAL_FORM_LANGUAGE']/../div/a/div[2]/div[2]/div[2]", 30);
+		if( lan != null){
+			Log.info("Language selected successfully!!!");
+			lan.click();
+		}else{
+			Log.error("drop down not visible");
+		}
+		driver.findElement(By.xpath(elementLocatorProp.getProperty("SAVE_BTN_XPATH"))).click();
+		Thread.sleep(10000);
+		refreshWebPage();
+		Thread.sleep(10000);
 	}
-	
-	
+
+	public static boolean labelsTestInPreferenceMainActivityPage()throws Exception{
+		
+		Log.info("Verfiying labels in preferences tab...");
+		
+		String expected[] = otherLangTestData.getProperty("LABELS_IN_PREF_TAB_PAGE_FRENCH").split(",");
+		List<String> lsLabelText = new ArrayList<String>();
+
+		List<WebElement> lsLabels = driver.findElements(By.tagName("label"));
+		for(WebElement label:lsLabels){
+			String temp = label.getText().trim();
+			if( !temp.isEmpty()){
+				lsLabelText.add(label.getText().trim());
+			}
+		}
+		String[] actuals = new String[lsLabelText.size()];
+		actuals = lsLabelText.toArray(actuals);
+
+		boolean stepResult = Verify.compareStringInNonEnglish(actuals, expected);
+		return stepResult;
+	}
 
 }
